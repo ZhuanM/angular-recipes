df4fbfbc6ec5e349252760293aa01276fe3b30dc
Zhuan <jowan@abv.bg>
2023-06-06 22:38:49 +0300

    Changed urls

--- src/app/shared/api-urls.ts
+++ src/app/shared/api-urls.ts
@@ -2,6 +2,9 @@ import { environment } from 'src/environments/environment';
 
 export const apiUrls = {
   // Authentication
-  loginUrl: environment.apiBaseUrl + 'auth/token',
-  registerUrl: environment.apiBaseUrl + 'api/user/register',
+  loginUrl: environment.apiBaseUrl + 'auth/initiate',
+  registerUrl: environment.apiBaseUrl + 'auth',
+  // Recipes
+  getRandomRecipesUrl: 'https://api.spoonacular.com/recipes/random?number=8', // Hardcoded to 8 results
+  favoriteRecipesUrl: environment.apiBaseUrl + 'favouriteRecipe', // GET, DELETE, POST
 }
