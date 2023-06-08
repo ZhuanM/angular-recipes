eadbb7e37e2c5f0ebda896e937ae19ecb491d95d
Zhuan <jowan@abv.bg>
2023-06-08 15:02:47 +0300

    Fixed configs

--- src/app/shared/api-urls.ts
+++ src/app/shared/api-urls.ts
@@ -4,9 +4,9 @@ export const apiUrls = {
   // Authentication
   loginUrl: environment.apiBaseUrl + 'auth/initiate',
   registerUrl: environment.apiBaseUrl + 'auth',
-  userUrl: environment.apiBaseUrl + 'user/id', // TODO CHANGE ENDPOINT FOR GET USER
+  userUrl: environment.apiBaseUrl + 'user/', // Expects id
   // Recipes
   getRandomRecipesUrl: 'https://api.spoonacular.com/recipes/random',
   getRecipeUrl: 'https://api.spoonacular.com/recipes/',
-  favoriteRecipesUrl: environment.apiBaseUrl + 'favouriteRecipe', // GET, DELETE, POST
+  favoriteRecipesUrl: environment.apiBaseUrl + 'recipes', // GET, DELETE, POST
 }
